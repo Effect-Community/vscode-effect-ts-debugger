0b950fbd246e91ae036b95498f716ac1a336e038
Michael Arnaldi <ma@matechs.com>
2022-11-28 12:38:09 +0000

    work in progress

--- sample-project/index.ts
+++ sample-project/index.ts
@@ -1,58 +1,17 @@
-import * as T from "@effect/core/io/Effect";
-import * as SUP from "@effect/core/io/Supervisor";
-import * as RC from "@effect/core/io/RuntimeConfig";
+import * as E from "@effect/io/Effect";
+import { pipe } from "@fp-ts/data/Function";
+import { seconds } from "@fp-ts/data/Duration";
 
-const sendToDebugger = (data: any) => {
-  debugger;
-};
-
-const supervisor = new SUP.Supervisor(
-  SUP.none.value,
-  SUP.none.unsafeOnStart,
-  SUP.none.unsafeOnEnd,
-  (fiber, eff) => {
-    const instruction: T.Instruction = eff as any;
-    const data = { fiberId: fiber._id.id, trace: instruction.trace };
-    const wrapContinuation =
-      <A>(obj: A) => <K extends keyof A>(key: K) => {
-        const f: any = obj[key]
-        obj[key] = ((...args: any[]) => {
-          sendToDebugger(data)
-          return f(...args)
-        }) as any
-      }
-
-    if (instruction._tag === "SucceedNow") {
-      sendToDebugger(data);
-    } else if (instruction._tag === "Succeed") {
-      wrapContinuation(instruction)("effect")
-    } else if (instruction._tag === "SucceedWith") {
-      wrapContinuation(instruction)("effect");
-    } else if (instruction._tag === "FlatMap") {
-      wrapContinuation(instruction)("k");
-    } else if (instruction._tag === "Fail") {
-      sendToDebugger(data)
-    }
-  },
-  SUP.none.unsafeOnSuspend,
-  SUP.none.unsafeOnResume
+pipe(
+  E.sync(() => Array.from({ length: 5 }, (_, n) => n)),
+  E.flatMap(E.forEachPar((n) => E.delay(seconds(2))(E.succeed(n + 1)))),
+  E.timed,
+  E.flatMap(([{ millis }]) => E.log(`timed: ${millis} ms`)),
+  E.tap(() =>
+    E.async<never, never, void>((cb) => {
+      setTimeout(() => cb(E.unit()), 100);
+    })
+  ),
+  E.withParallelism(2),
+  E.unsafeFork
 );
-
-/**
- * @tsplus fluent ets/Effect enableDebugger
- */
-export function enableDebugger<R, E, A>(
-  effect: T.Effect<R, E, A>
-): T.Effect<R, E, A> {
-  return effect.modifyRuntimeConfig((runtime) =>
-    RC.superviseOperations(runtime).addSupervisor(supervisor)
-  );
-}
-
-const sample = T.succeed<number>(1)
-  .map((e) => e * 2)
-  .flatMap(a => T.fail(a))
-  .flatMap((e) => T.succeed(e * 2))
-  .enableDebugger();
-
-T.unsafeRunAsync(sample);
--- src/extension.ts
+++ src/extension.ts
@@ -1,180 +1,26 @@
 import * as vscode from "vscode";
 
-let effectDebuggerStep = false;
+let effectDebuggerMode = true;
 
-function handleError(error: any) {
-  console.log(error);
-}
-
-/**
- * This is the event triggered by the DAP when the code execution is stopped
- */
 interface VSCodeDebuggerStoppedEvent {
   type: "event";
   event: "stopped";
 }
-const isVSCodeDebuggerStoppedEvent = (
-  m: any
-): m is VSCodeDebuggerStoppedEvent =>
+
+const isVSCodeDebuggerStoppedEvent = (m: any): m is VSCodeDebuggerStoppedEvent =>
   m && m.type === "event" && m.event === "stopped";
-
-/**
- * Ensure a uri is a real existing file.
- * This is used to avoid loading packages with (@effect/module) in the path
- * eventually in the future we will find a way to get the real file path
- */
-async function fileUriExists(jsUri: vscode.Uri) {
-  try {
-    const data = await vscode.workspace.fs.stat(jsUri);
-    return data.type !== vscode.FileType.Directory;
-  } catch (e) {}
-  return false;
-}
-
-/**
- * Parse file:row:column string into its components
- */
-function parseFileRowColumnFromTrace(
-  traceString: string
-): null | readonly [string, number, number] {
-  const match = /^(\(.[^\)]+\)\s*)*(.+)\:(\d+)\:(\d+)$/ig.exec(traceString)
-  if(match === null) return match
-  const row = parseInt(match[3], 10) - 1;
-  const column = parseInt(match[4], 10);
-  const file = ''+match[2];
-  return [file, row, column];
-}
-
-interface EffectTsDebugInfo {
-  fiberId: string;
-  trace: string | null;
-}
-
-/**
- * Asks current effect-ts info to the current debug session
- */
-async function getEffectTsDebugInfo(
-  session: vscode.DebugSession
-): Promise<EffectTsDebugInfo | null> {
-  try {
-    // first we need to get the current frame
-    const resThreads = await session.customRequest("threads", {});
-    for (const thread of resThreads.threads) {
-      const resStackTrace = await session.customRequest("stackTrace", {
-        threadId: thread.id,
-      });
-
-      // then we get the current fiber being evaluated
-      const unsafeCurrentFiberDebugInfoExpression = `JSON.stringify(arguments[0])`;
-
-      const resUnsafeFiber = await session.customRequest("evaluate", {
-        expression: unsafeCurrentFiberDebugInfoExpression,
-        frameId: resStackTrace.stackFrames[0].id,
-        context: "clipboard",
-      });
-      const resultJson = JSON.parse(resUnsafeFiber.result);
-      const result: EffectTsDebugInfo | null = JSON.parse(resultJson);
-      return result;
-    }
-  } catch (e) {
-    handleError(e);
-  }
-  return null;
-}
-
-/**
- * Given a debug session and a effect-ts trace, try to resolve a valid vscode location
- */
-async function getLocationFromTraceElement(
-  session: vscode.DebugSession,
-  trace: string | null
-): Promise<vscode.Location | null> {
-  try {
-    if (!trace) return null
-    const parsedTrace = parseFileRowColumnFromTrace(trace)
-    if(!parsedTrace) return null
-    const [file, row, column] = parsedTrace;
-    //const absolutePath = session.workspaceFolder?.uri.toString() + "/" + file;
-
-    const fileUri = vscode.Uri.file(file)
-    const position = new vscode.Position(row, column);
-    const location = new vscode.Location(fileUri, position);
-
-    return location;
-  } catch (e) {
-    handleError(e);
-  }
-  return null;
-}
-
-/**
- * Move the editor cursor to the given location, opening a file if necessary.
- */
-async function moveEditorPointerAtLocation(
-  location: vscode.Location
-): Promise<vscode.TextEditor | null> {
-  try {
-    const document = await vscode.workspace.openTextDocument(location.uri);
-    const editor = await vscode.window.showTextDocument(document, 1, false);
-    editor.selection = new vscode.Selection(
-      location.range.start,
-      location.range.end
-    );
-    editor.revealRange(
-      location.range,
-      vscode.TextEditorRevealType.InCenterIfOutsideViewport
-    );
-    return editor;
-  } catch (e) {
-    handleError(e);
-  }
-  return null;
-}
 
 const liveLogDecorationType = vscode.window.createTextEditorDecorationType({
   after: {
     margin: "0 0 0 3em",
     color: new vscode.ThemeColor("effectTsDebugger.inlineInfoForegroundColor"),
-    backgroundColor: new vscode.ThemeColor(
-      "effectTsDebugger.inlineInfoBackgroundColor"
-    ),
+    backgroundColor: new vscode.ThemeColor("effectTsDebugger.inlineInfoBackgroundColor"),
   },
   rangeBehavior: vscode.DecorationRangeBehavior.ClosedClosed,
 });
 
 let lastInlineInfoEditor: vscode.TextEditor | undefined = undefined;
-/**
- * Shows inline info when the debugger breaks
- */
-async function setInlineInfo(
-  editor: vscode.TextEditor,
-  location: vscode.Location,
-  info: EffectTsDebugInfo
-) {
-  try {
-    clearInlineInfo();
-    const livePosition = new vscode.Range(
-      location.range.start.line,
-      Number.MAX_SAFE_INTEGER,
-      location.range.start.line,
-      Number.MAX_SAFE_INTEGER
-    );
-    const contentText = "Fiber #" + info.fiberId;
-    editor.setDecorations(liveLogDecorationType, [
-      {
-        range: livePosition,
-        renderOptions: { after: { contentText } },
-      },
-    ]);
-    lastInlineInfoEditor = editor;
-  } catch (e) {
-    handleError(e);
-  }
-}
 
-/**
- * Clear the current inline info
- */
 async function clearInlineInfo() {
   if (lastInlineInfoEditor) {
     lastInlineInfoEditor.setDecorations(liveLogDecorationType, []);
@@ -182,50 +28,88 @@ async function clearInlineInfo() {
   lastInlineInfoEditor = undefined;
 }
 
-/**
- * Jump to current effect-ts trace
- */
-async function jumpToEffectTrace(
-  session: vscode.DebugSession
-): Promise<readonly [vscode.TextEditor, vscode.Location] | null> {
-  try {
-    await clearInlineInfo();
-    const effectInfo = await getEffectTsDebugInfo(session);
-    if (effectInfo) {
-      const sourceLocation =
-        effectInfo.trace;
-      const location = await getLocationFromTraceElement(
-        session,
-        sourceLocation
-      );
-      if (location) {
-        if (!(await fileUriExists(location.uri))) {
-          return await session.customRequest("continue");
+async function getEffectTsDebugInfo(session: vscode.DebugSession): Promise<void> {
+  const thread = (await session.customRequest("threads", {})).threads[0];
+  const stack = await session.customRequest("stackTrace", { threadId: thread.id });
+
+  for (const frame of stack.stackFrames) {
+    if (frame.presentationHint && frame.presentationHint === "label") {
+      return;
+    }
+    if (frame.name === "runLoop") {
+      try {
+        const { variablesReference: fiberIdRef } = await session.customRequest("evaluate", {
+          frameId: frame.id,
+          expression: "this.id()",
+        });
+
+        if (fiberIdRef) {
+          const { variables: fiberIdFields } = await session.customRequest("variables", {
+            variablesReference: fiberIdRef,
+          });
+          const fiberId = {
+            id: fiberIdFields.find((_: any) => _.name === "id").value,
+            startTimeMillis: fiberIdFields.find((_: any) => _.name === "startTimeMillis").value,
+          };
+          if (typeof fiberId.id === "undefined" || typeof fiberId.startTimeMillis === "undefined") {
+            return;
+          }
+          const position = new vscode.Position(
+            stack.stackFrames[0].line,
+            stack.stackFrames[0].column
+          );
+          const location = new vscode.Location(stack.stackFrames[0].source.path, position);
+          const editor = vscode.window.activeTextEditor!;
+          editor.revealRange(location.range, vscode.TextEditorRevealType.InCenterIfOutsideViewport);
+          const livePosition = new vscode.Range(
+            location.range.start.line - 1,
+            Number.MAX_SAFE_INTEGER,
+            location.range.start.line - 1,
+            Number.MAX_SAFE_INTEGER
+          );
+          const contentText = `Fiber #${fiberId.id} (started at: ${new Date(
+            JSON.parse(fiberId.startTimeMillis)
+          ).toISOString()})`;
+          editor.setDecorations(liveLogDecorationType, [
+            {
+              range: livePosition,
+              renderOptions: { after: { contentText } },
+            },
+          ]);
+          lastInlineInfoEditor = editor;
+          return;
         }
-        const editor = await moveEditorPointerAtLocation(location);
-        if (editor) {
-          await setInlineInfo(editor, location, effectInfo);
-          return [editor, location];
+      } catch (e) {
+        if (
+          typeof e === "object" &&
+          e &&
+          "message" in e &&
+          typeof e["message"] === "string" &&
+          e["message"].includes("async stack frame")
+        ) {
+          return;
+        } else {
+          throw e;
         }
       }
     }
-  } catch (e) {
-    handleError(e);
   }
-  return null;
+}
+
+async function augmentDebugInfo(session: vscode.DebugSession) {
+  clearInlineInfo();
+  await getEffectTsDebugInfo(session);
 }
 
 let statusBarItem: vscode.StatusBarItem | undefined;
 function updateStatusBarItem() {
   if (!statusBarItem) {
-    statusBarItem = vscode.window.createStatusBarItem(
-      vscode.StatusBarAlignment.Right
-    );
+    statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
     statusBarItem.command = "vscode-effect-ts-debugger.toggleDebuggerStep";
     statusBarItem.show();
   }
   if (statusBarItem) {
-    statusBarItem.text = "Effect-TS: " + (effectDebuggerStep ? "On" : "Off");
+    statusBarItem.text = "Effect-TS: " + (!effectDebuggerMode ? "Off" : "On");
   }
 }
 
@@ -240,12 +124,8 @@ export function activate(context: vscode.ExtensionContext) {
           clearInlineInfo();
         },
         onDidSendMessage: async (m) => {
-          try {
-            if (effectDebuggerStep && isVSCodeDebuggerStoppedEvent(m)) {
-              await jumpToEffectTrace(session);
-            }
-          } catch (e) {
-            handleError(e);
+          if (effectDebuggerMode && isVSCodeDebuggerStoppedEvent(m)) {
+            await augmentDebugInfo(session);
           }
         },
       };
@@ -257,10 +137,11 @@ export function activate(context: vscode.ExtensionContext) {
   disposable = vscode.commands.registerCommand(
     "vscode-effect-ts-debugger.toggleDebuggerStep",
     () => {
-      effectDebuggerStep = !effectDebuggerStep;
+      effectDebuggerMode = !effectDebuggerMode;
       updateStatusBarItem();
     }
   );
+
   context.subscriptions.push(disposable);
 
   disposable = vscode.commands.registerCommand(
@@ -268,7 +149,7 @@ export function activate(context: vscode.ExtensionContext) {
     () => {
       const debugSession = vscode.debug.activeDebugSession;
       if (debugSession) {
-        jumpToEffectTrace(debugSession);
+        augmentDebugInfo(debugSession);
       }
     }
   );
@@ -277,5 +158,4 @@ export function activate(context: vscode.ExtensionContext) {
   updateStatusBarItem();
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
